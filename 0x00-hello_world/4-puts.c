a8110341bb323663201d72f8ac3e555426b6b3c2
daadesina <daadesina1@gmail.com>
2023-10-07 10:37:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,7 @@
-#!/bin/bash
-char my_word[] = '"Programming is like building a multilingual puzzle';
-puts(my_word);
+#include <stdio.h>
+int main(void)
+{
+	char my_word[] = "\"Programming is like building a multilingual puzzle";
+	puts(my_word);
+	return (0);
+}
