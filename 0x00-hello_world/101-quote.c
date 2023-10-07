4be497fbdc560833ffce82ab33b0d59041f78799
daadesina <daadesina1@gmail.com>
2023-10-07 14:21:50 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry mode
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
