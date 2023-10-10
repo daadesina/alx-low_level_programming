08304cb15c941a35682963f21fd12507554efaff
daadesina <daadesina1@gmail.com>
2023-10-10 14:48:44 -0700

    modify

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,11 +11,13 @@ int _abs(int r)
 {
 	if (r >= '0')
 	{
-		printf("%d\n", r);
+		printf("%d", r);
+		return ('\n');
 	}
 	else
 	{
 		int n = (-1 * r);
 		printf("%d\n", n);
+		return ('\n');
 	}
 }
