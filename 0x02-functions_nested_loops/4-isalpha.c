6ef18d714f663044f75ab19f3cabedc8fba81e45
daadesina <daadesina1@gmail.com>
2023-10-10 13:48:49 -0700

    isalph

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabet
+ * @c: any alphabet
+ *
+ * Return: 1 if alphabet, 0 if not alphabet
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
