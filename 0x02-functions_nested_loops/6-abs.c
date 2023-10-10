9bfa68bdbbb20f2b2676763d436d6091b5423ef7
daadesina <daadesina1@gmail.com>
2023-10-10 14:32:09 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - chech Absolut value
+ * @r: number
+ *
+ * Return: The absolut number
+ */
+int _abs(int)
+{
+	int r;
+
+	if (r >= '0')
+	{
+		printf("%d\n", r);
+	}
+	else
+	{
+		int n = ('-' * r);
+		printf("%d\n", n);
+	}
+	return (0);
+}
