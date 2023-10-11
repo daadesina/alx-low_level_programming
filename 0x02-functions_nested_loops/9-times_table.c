217c193b1153e3093ec081fa702c1c4101988621
daadesina <daadesina1@gmail.com>
2023-10-11 12:19:51 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,19 +2,27 @@
 #include "main.h"
 
 /**
- * times_table - Entry point
+ * times_table - Entry mode
  *
  * Return: Nothing to return
  */
 void times_table(void)
 {
-	int a = 0;
+	int a;
+	int count = 0;
 
-	while (a <= 12)
+	while (count <= 9)
 	{
-		int b = 3 * a;
+		a = 0;
 
-		printf("%d, ", b);
-		a++;
+		while (a <= 12)
+		{
+			int b = count * a;
+
+			printf("%d, ", b);
+			a++;
+		}
+		_putchar('\n');
+		count++;
 	}
 }
