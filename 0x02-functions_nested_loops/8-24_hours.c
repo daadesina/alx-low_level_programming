f394849cfccabb3c4d40dc7d2623b28569281c10
daadesina <daadesina1@gmail.com>
2023-10-11 07:26:06 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Any value
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d = '0';
+
+	while (d <= '2')
+	{
+		c = '0';
+
+		while (c <= '3')
+		{
+			b = '0';
+
+			while (b <= '5')
+			{
+				a = '0';
+
+				while (a <= '9')
+				{
+					_putchar(d);
+					_putchar(c);
+					_putchar(':');
+					_putchar(b);
+					_putchar(a);
+					_putchar('\n');
+					a++;
+				}
+				b++;
+			}
+			c++;
+		}
+		d++;
+	}
+}
