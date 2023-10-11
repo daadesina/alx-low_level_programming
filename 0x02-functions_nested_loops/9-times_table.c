59be8bc64ed378b38a0f0c5cf2f4e50675661927
daadesina <daadesina1@gmail.com>
2023-10-11 11:22:28 -0700

    done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Return: Nothing to return
+ */
+void times_table(void)
+{
+	int a = 0;
+
+	while (a <= 12)
+	{
+		int b = 3 * a;
+
+		printf("%d, ", b);
+		a++;
+	}
+}
