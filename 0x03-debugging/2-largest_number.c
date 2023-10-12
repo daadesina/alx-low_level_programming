795ee7a4741b3c927f4133a829722ce97418ec7f
daadesina <daadesina1@gmail.com>
2023-10-12 07:26:41 -0700

    nice

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,16 +15,14 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		return (largest);
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		return (largest);
 	}
 	else
 	{
 		largest = c;
-		return (largest);
 	}
+	return (largest);
 }
