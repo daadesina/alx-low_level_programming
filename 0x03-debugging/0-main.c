6ed150403fa7018d23a3a8d372e0133a2539efc3
daadesina <daadesina1@gmail.com>
2023-10-12 01:08:41 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - testing the code
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postitive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory was created on the 12th of october, 2023
