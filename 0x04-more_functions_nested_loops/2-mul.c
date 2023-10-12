dc8e1e4432bbf8fd32323fbc4b66979f235644d1
daadesina <daadesina1@gmail.com>
2023-10-12 08:24:24 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Starting point
+ * @a: a nunber
+ * @b: another number
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
