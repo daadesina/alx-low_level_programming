6613fcb6624dca7c0bb26aee96e4733c7446677a
daadesina <daadesina1@gmail.com>
2023-10-13 06:13:34 -0700

    adesina

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Nothing to return
+ */
+void more_numbers(void)
+{
+	int count = 0;
+	int a = 0;
+
+	while (count < 10)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+			{
+				_putchar('1');
+			}
+			_putchar((a % 10) + '0');
+			a++;
+		}
+		_putchar('\n');
+	}
+}
