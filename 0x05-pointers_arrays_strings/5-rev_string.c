6242aa73f4a4289dabcb2a2a676be9180e812de0
daadesina <daadesina1@gmail.com>
2023-10-16 15:35:42 -0700

    adesina

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * rev_string - Entry point
- * @s: a string
- * Return: Nothing to return
+ * rev_string -> reversing a string
+ * @s: parameter s
  */
 void rev_string(char *s)
 {
-	int real, roll;
+	char tmp;
+	int i, length1, length2;
 
-	roll = 0;
-	while (s[roll] != '\0')
-	{
-		s[roll];
-		roll++;
-	}
-	real = roll - 1;
-	while (real >= 0)
-	{
+	length1 = 0;
+	length2 = 0;
 
-		s[real];
-		real--;
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	}
 }
