4564cdb316013d40931e9426eb74b931359465d0
daadesina <daadesina1@gmail.com>
2023-10-16 15:32:04 -0700

    adesina

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,15 +12,14 @@ void rev_string(char *s)
 	roll = 0;
 	while (s[roll] != '\0')
 	{
-		_putchar(s[roll]);
+		s[roll];
 		roll++;
 	}
-	_putchar('\n');
 	real = roll - 1;
 	while (real >= 0)
 	{
-		_putchar(s[real]);
+
+		s[real];
 		real--;
 	}
-	_putchar('\n');
 }
