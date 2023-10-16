51021c02e120aecc34897fea9ad87b57f274be4f
daadesina <daadesina1@gmail.com>
2023-10-16 14:10:19 -0700

    adesina

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: a pinter
+ * Return: The result
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory was created on the 16th of October, 2023
