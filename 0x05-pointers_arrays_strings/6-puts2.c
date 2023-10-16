c7f87694044f2e9e73a82fcea337bd6a9d34a8cb
daadesina <daadesina1@gmail.com>
2023-10-16 15:40:44 -0700

    adesina

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: string
+ * Return: the result
+ */
+void puts2(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s = s + 2;
+	}
+}
