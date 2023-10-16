e27d22c85c9415f41603ed6fcbc6d56839fa96e1
daadesina <daadesina1@gmail.com>
2023-10-16 14:49:05 -0700

    adesina

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: a string;
+ * Return: the result
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+	}
+	_putchar('\n');
+}
