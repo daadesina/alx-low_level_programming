7120015775aae592d6755202b9f4efcc0c0c5de4
daadesina <daadesina1@gmail.com>
2023-10-18 15:48:58 -0700

    adesina

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Entry point
+ * @s1: a character
+ * @s2: another character
+ * Return: the result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
