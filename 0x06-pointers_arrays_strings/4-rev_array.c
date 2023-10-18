0293040aeb64dbbe4d11f7f1ba431ba031d96557
daadesina <daadesina1@gmail.com>
2023-10-18 16:05:37 -0700

    adesina

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: an integer
+ * @n: another integer
+ * Return: Nothing to return
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i != n)
+	{
+		i++;
+	}
+	while (j < i && j >= 0)
+	{
+		a[j];
+		j--;
+	}
+}
