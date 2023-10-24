4f25b413f3364ffecd2d7dc2c5e222a3bf521f3e
daadesina <daadesina1@gmail.com>
2023-10-24 07:41:13 -0700

    adesina

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,6 +11,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, n);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 	return (dest);
 }
