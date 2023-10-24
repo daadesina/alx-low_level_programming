babd00a98f117fc7d2f5e8d33b1583aa1730c643
daadesina <daadesina1@gmail.com>
2023-10-24 03:32:07 -0700

    adesina

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	int count = 0;
 
-	memset(*s, b, sizeof(char) * n);
-	while (i < n)
+	while (count < n)
 	{
-		return (s[i]);
-		i++;
+		s[count] = b;
+		count++;
+		n--;
 	}
+	return (s);
 }
