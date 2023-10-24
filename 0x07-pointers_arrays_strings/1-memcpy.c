abb318c1c974cd465913d571b30bd83dfbf5ba54
daadesina <daadesina1@gmail.com>
2023-10-24 07:29:34 -0700

    adesina

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - Entry point
+ * @dest: a character
+ * @src: a character
+ * @n: an integer
+ * Result: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+
+	return (dest);
+}
