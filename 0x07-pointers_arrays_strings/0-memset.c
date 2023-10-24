fb26c2c2228fd9b33cb5b91cbde235d20bf7b5f7
daadesina <daadesina1@gmail.com>
2023-10-24 03:42:49 -0700

    adesina

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,12 +6,13 @@
  * @s: a pointer character
  * @b: a character
  * @n: an integer
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count = 0;
 
-	for (count = 0; n > 0; i++; n--;)
+	for (count = 0; n > 0; count++; n--;)
 	{
 		s[count] = b;
 	}
