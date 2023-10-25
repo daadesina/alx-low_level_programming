fc330f2c3d392286812975e35c733c23b9ad044d
daadesina <daadesina1@gmail.com>
2023-10-25 13:03:14 -0700

    adesina

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: an integer
+ * Return: the factorial calculation
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
