69d15d45f2ea0741077acff5355f6529a97ec32f
daadesina <daadesina1@gmail.com>
2023-10-25 12:06:10 -0700

    adesina

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s: a character
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
