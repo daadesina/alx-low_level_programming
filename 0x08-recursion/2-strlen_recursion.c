a1167d66607fa41794845f5ae95dd03306a61b8e
daadesina <daadesina1@gmail.com>
2023-10-25 12:42:19 -0700

    adesina

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,8 +7,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	if (*s != '\0')	
+	if (*s == '\0')	
 	{
-		len = *s;
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
