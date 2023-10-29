5dcd69a0a147da84e35752be157f2c5c81f5478b
daadesina <daadesina1@gmail.com>
2023-10-29 13:22:31 -0700

    adesina

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int __attribute__ ((unused))main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
