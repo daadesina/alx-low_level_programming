e063e3f351ad5ab3a5ebe0b5ffa81486bbba56cf
daadesina <daadesina1@gmail.com>
2023-10-29 14:09:05 -0700

    adesina

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int mut;
+
+	if (argc == 3)
+	{
+		mut = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mut);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
