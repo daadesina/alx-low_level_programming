dd8347b5e2d6d4b79c8802d9af82b41353b0f79c
daadesina <daadesina1@gmail.com>
2023-10-31 14:46:44 -0700

    done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Entry point
+ * @s1: string
+ * @s2: another string
+ * Return: the result
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, new_str_len, counter;
+	char *new_str;
+
+	if (s1 == NULL)
+		s1 = "";
+	If (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[1] != '\0';  i++)
+		continue;
+	for (j = 0; s2[j] != '\0'; j++)
+		continue;
+
+	new_str_len = (i + j) + 1;
+
+	new_str = malloc(new_str_len);
+	if (new_str == NULL)
+		return (NULL);
+
+	counter = 0;
+
+	for (k = 0; k < i; k++)
+	{
+		new_str[counter] = s1[k];
+		counter++;
+	}
+	for (k = 0; k < j; k++)
+	{
+		new_str[counter] = s2[k];
+		counter++;
+	}
+
+	new_str[count] = '\0';
+
+	return (new_str);
+}
