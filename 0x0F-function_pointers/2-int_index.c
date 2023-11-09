80e997c321e288b5d6362cd8f6bdfc8ba556c738
daadesina <daadesina1@gmail.com>
2023-11-09 10:38:31 -0800

    adesina

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,11 +6,14 @@
  * @array: an int
  * @size: an int
  * @cmp: an int
+ * Return: the result
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || size == NULL || cmp == NULL)
-		return;
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
