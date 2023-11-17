c698d8cd393a4e8ddb3fd46797c462409f075834
daadesina <daadesina1@gmail.com>
2023-11-17 06:07:50 -0800

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_list - print the list
+ * @list_t: a constant
+ * @h a constant variable
+ * Retrun: nober of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	if (str == NULL)
+		printf("[0] (nil)");
+	else
+	{
+		return (len);
+	}
+}
