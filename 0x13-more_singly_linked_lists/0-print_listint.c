c2a7b18efb92fad27b42fc1dd15d815de051fc28
daadesina <daadesina1@gmail.com>
2023-11-20 13:56:32 -0800

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 	num = 0;
 	while (h != NULL)
 	{
+		printf("%d\n", h->n);
+		h = h->next;
 		num++;
-		printf("%d\n", h -> n);
-		h = h -> next;
 	}
 	return (num);
 }
